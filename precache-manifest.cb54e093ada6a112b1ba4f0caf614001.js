f382b31a1854865dd8920ce4be4ec7d1f975e56b
Lê Võ Hoàng Phi <philevohoang@gmail.com>
2019-11-27 13:45:25 +0700

    web

---
+++ precache-manifest.cb54e093ada6a112b1ba4f0caf614001.js
@@ -0,0 +1,306 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a0264982088b2d51a676452ffc4b1e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "573f10445cb272ebcbf6",
+    "url": "/static/css/2.30861e7f.chunk.css"
+  },
+  {
+    "revision": "d7fa9ab299ac0bdc6a30",
+    "url": "/static/css/main.85c3f80e.chunk.css"
+  },
+  {
+    "revision": "573f10445cb272ebcbf6",
+    "url": "/static/js/2.4e873324.chunk.js"
+  },
+  {
+    "revision": "d7fa9ab299ac0bdc6a30",
+    "url": "/static/js/main.ff4f573b.chunk.js"
+  },
+  {
+    "revision": "233e31c1ded7ff278f89",
+    "url": "/static/js/runtime-main.9d57ae99.js"
+  },
+  {
+    "revision": "72f220cd3e521bdb2d40b205e98e0a77",
+    "url": "/static/media/Budweiser.72f220cd.png"
+  },
+  {
+    "revision": "d1a0f34eec845100c0c9664d5f3503da",
+    "url": "/static/media/address_icon_24x24.d1a0f34e.svg"
+  },
+  {
+    "revision": "aa6494a7865b884153f4693701f89c9d",
+    "url": "/static/media/adult_icon_32x32.aa6494a7.svg"
+  },
+  {
+    "revision": "fe43de7905f6475552e2a4aca3840c50",
+    "url": "/static/media/arrow_left.fe43de79.svg"
+  },
+  {
+    "revision": "79b26cb4f9cf3e3467c7f3d28471de69",
+    "url": "/static/media/arrow_right.79b26cb4.svg"
+  },
+  {
+    "revision": "4713f1c6f00eb6301caac5a7521f5490",
+    "url": "/static/media/background.4713f1c6.svg"
+  },
+  {
+    "revision": "3f470916377d683a4938f2e40581d104",
+    "url": "/static/media/bamboo_icon_400x400.3f470916.svg"
+  },
+  {
+    "revision": "3609b48654a5e514f84442ac70a845bb",
+    "url": "/static/media/banhang_dangchothanhtoan_icon_42x42.3609b486.svg"
+  },
+  {
+    "revision": "ea64dde0c295a30208dffbd9745548d6",
+    "url": "/static/media/banhang_lichsuthanhtoan_icon_42x42.ea64dde0.svg"
+  },
+  {
+    "revision": "b9e14c3cab3d029705ce23c6ee47599b",
+    "url": "/static/media/banhang_maqr_icon_42x42.b9e14c3c.svg"
+  },
+  {
+    "revision": "f21f6c81fe44ea97d84e8441036c6c07",
+    "url": "/static/media/banhang_maqr_white.f21f6c81.svg"
+  },
+  {
+    "revision": "9b4884b246c391977e54115ec9b50a45",
+    "url": "/static/media/banhang_quanlycuahang_icon_42x42.9b4884b2.svg"
+  },
+  {
+    "revision": "82a4e93e4246c112b1b5e3a052c3375c",
+    "url": "/static/media/banhang_quanlydanhmuc_icon_42x42.82a4e93e.svg"
+  },
+  {
+    "revision": "4976562d3eb5208dbc38f09d47531dc0",
+    "url": "/static/media/banhang_quanlynhanvien_icon_42x42.4976562d.svg"
+  },
+  {
+    "revision": "e5be320359c1467c1e2b5bdd06ccbe40",
+    "url": "/static/media/banhang_quanlysanpham_icon_42x42.e5be3203.svg"
+  },
+  {
+    "revision": "59cf25b954ac8756bbf38bf57d12be7a",
+    "url": "/static/media/banhang_taodonhang_icon_42x42.59cf25b9.svg"
+  },
+  {
+    "revision": "be0f77dd61471454c847a43197ecebc2",
+    "url": "/static/media/banhang_white.be0f77dd.svg"
+  },
+  {
+    "revision": "e019e78ef00a3bcb6b6eea37893b3f55",
+    "url": "/static/media/bookflight_icon_32x32.e019e78e.svg"
+  },
+  {
+    "revision": "f5c6ed6bd0dc60f05a347cca58a5d231",
+    "url": "/static/media/box_bg.f5c6ed6b.svg"
+  },
+  {
+    "revision": "07489ac3f7c288815f0b871716d80b43",
+    "url": "/static/media/children_icon_32x32.07489ac3.svg"
+  },
+  {
+    "revision": "57bbd58af9398f3df4181aee74d16e03",
+    "url": "/static/media/choose_from_icon_30x30.57bbd58a.svg"
+  },
+  {
+    "revision": "82a5595628ed708ce0589ce553ff4767",
+    "url": "/static/media/cmnd_icon_24x24.82a55956.svg"
+  },
+  {
+    "revision": "fb2e3db6cc9c09028574c82b4ec43a73",
+    "url": "/static/media/company_name_icon_24x24.fb2e3db6.svg"
+  },
+  {
+    "revision": "5eb54ba28101d3b6212ea0100dc16984",
+    "url": "/static/media/delete_circle_32x32.5eb54ba2.svg"
+  },
+  {
+    "revision": "e252ee28249cf43212400e29b66f1de2",
+    "url": "/static/media/detail_icon_32x32.e252ee28.svg"
+  },
+  {
+    "revision": "1cccb19472ba37a5e15773a525503b6f",
+    "url": "/static/media/ecm_logo.1cccb194.svg"
+  },
+  {
+    "revision": "52a846558ce60d0fccc2a4212a04a0b0",
+    "url": "/static/media/edit_circle_32x32.52a84655.svg"
+  },
+  {
+    "revision": "97835c8a82077804fc57ed9aea666879",
+    "url": "/static/media/evn-hanoi.97835c8a.svg"
+  },
+  {
+    "revision": "a61980b9c46dcfbe553237eec484a967",
+    "url": "/static/media/evn-hcm.a61980b9.svg"
+  },
+  {
+    "revision": "b9af454ff5bdb2c058b01e6961cb3177",
+    "url": "/static/media/evn-mienbac.b9af454f.svg"
+  },
+  {
+    "revision": "30f478dbfe05fa9e5133460c31465973",
+    "url": "/static/media/evn-miennam.30f478db.svg"
+  },
+  {
+    "revision": "7f38c993c4787d6519a8177a449d0c87",
+    "url": "/static/media/evn-mientrung.7f38c993.svg"
+  },
+  {
+    "revision": "f6b33c04a18964755d07d4b6b13cc2bf",
+    "url": "/static/media/fail_flow_login.f6b33c04.svg"
+  },
+  {
+    "revision": "1ed348ea2ba9e0371d07b0163b45bf90",
+    "url": "/static/media/icon-close-pop-up.1ed348ea.svg"
+  },
+  {
+    "revision": "3bf647e2e2090113f9c8491fec60aa5f",
+    "url": "/static/media/icon-close.3bf647e2.svg"
+  },
+  {
+    "revision": "9d82ca2ccdf4d39821e5da22800e64c7",
+    "url": "/static/media/icon_banhang_inactive.9d82ca2c.svg"
+  },
+  {
+    "revision": "bc62d6762b2e55c8d0ef85749ff141f3",
+    "url": "/static/media/icon_baocao_inactive.bc62d676.svg"
+  },
+  {
+    "revision": "23e4f47ffd596fd5204164be4fb7b4cd",
+    "url": "/static/media/icon_thuho_inactive.23e4f47f.svg"
+  },
+  {
+    "revision": "ab4c526777c3ce408d0cb116f1758202",
+    "url": "/static/media/icons_search.ab4c5267.svg"
+  },
+  {
+    "revision": "f52cdd64dd78f94d6b1f881bfb181eb2",
+    "url": "/static/media/infant_icon_32x32.f52cdd64.svg"
+  },
+  {
+    "revision": "2e1dd9045f6e3823a6fea18895aba9a2",
+    "url": "/static/media/jetstar_icon_400x400.2e1dd904.svg"
+  },
+  {
+    "revision": "698955d02f9a93eb25f395773934524e",
+    "url": "/static/media/location_icon_24x24.698955d0.svg"
+  },
+  {
+    "revision": "63bde7e89a28b7488ddf050c6a40f5c1",
+    "url": "/static/media/logo_white.63bde7e8.svg"
+  },
+  {
+    "revision": "5211047b95bef40e8eff1fe684caee4c",
+    "url": "/static/media/logout_icon.5211047b.svg"
+  },
+  {
+    "revision": "a41d89a1e0406938c718ad2f1cca6efc",
+    "url": "/static/media/muave_icon_100x90.a41d89a1.svg"
+  },
+  {
+    "revision": "8a4bcd5fee78e10c9b7ee727a0bb25fb",
+    "url": "/static/media/mybooking_icon_100x90.8a4bcd5f.svg"
+  },
+  {
+    "revision": "eced3d36e883e0bf5d58d0d5c3e9546b",
+    "url": "/static/media/myclient_icon_100x90.eced3d36.svg"
+  },
+  {
+    "revision": "0a9c2ce0fd77e319a62d5992575d2eef",
+    "url": "/static/media/pass_icon.0a9c2ce0.svg"
+  },
+  {
+    "revision": "3950ef45dde5e92878822876fb2370f6",
+    "url": "/static/media/paybycash_icon_32x32.3950ef45.svg"
+  },
+  {
+    "revision": "134ff8330af7bfba9ebb3544557806ed",
+    "url": "/static/media/phone_icon.134ff833.svg"
+  },
+  {
+    "revision": "1fe5a45702387b4a3a7605564cdc97aa",
+    "url": "/static/media/pin_code.1fe5a457.svg"
+  },
+  {
+    "revision": "e6bc76635b46e15e0df19bdae9a1a7b8",
+    "url": "/static/media/profile_icon_white.e6bc7663.svg"
+  },
+  {
+    "revision": "e0080d0bb173483a97e43fac9d8a0dda",
+    "url": "/static/media/qrcode_white.e0080d0b.svg"
+  },
+  {
+    "revision": "14756a8404e9b2025f8b8ab2e2deaf19",
+    "url": "/static/media/settings_white.14756a84.svg"
+  },
+  {
+    "revision": "adf0324a3f78ac9004b6c29071c15680",
+    "url": "/static/media/success_green.adf0324a.svg"
+  },
+  {
+    "revision": "cdd7b291f62f90344d1cd360f8aef28f",
+    "url": "/static/media/thuho_all_icon.cdd7b291.svg"
+  },
+  {
+    "revision": "5a3e014af2fd8c2852489cd600dfc9f7",
+    "url": "/static/media/thuho_baohiem_icon.5a3e014a.svg"
+  },
+  {
+    "revision": "ee92ad756a97b0ceaccf22d125554c6f",
+    "url": "/static/media/thuho_dienthoai_icon.ee92ad75.svg"
+  },
+  {
+    "revision": "4766cae382943891175383550b69566a",
+    "url": "/static/media/thuho_game_icon.4766cae3.svg"
+  },
+  {
+    "revision": "d932e7d9f8ee6ec14919134503fbfe0c",
+    "url": "/static/media/thuho_hocphi_icon.d932e7d9.svg"
+  },
+  {
+    "revision": "b89c87a423d920ccc208e0a9ae695a97",
+    "url": "/static/media/thuho_tiendien_icon.b89c87a4.svg"
+  },
+  {
+    "revision": "ee40f7ab82e7124e011800a8e5e81754",
+    "url": "/static/media/thuho_tiennuoc_icon.ee40f7ab.svg"
+  },
+  {
+    "revision": "e98e862e6c75d19e2bbe887b13fe472c",
+    "url": "/static/media/thuho_truyenhinh_icon.e98e862e.svg"
+  },
+  {
+    "revision": "fd7633b91677bf248d8e5114ed62ca11",
+    "url": "/static/media/thuho_vemaybay_icon_46x46.fd7633b9.svg"
+  },
+  {
+    "revision": "00fac812d48f5ffe9f1c3dc72b4eba34",
+    "url": "/static/media/tiger.00fac812.png"
+  },
+  {
+    "revision": "bf841569495823e914941b3046e933e9",
+    "url": "/static/media/tongquan_icon_inactive.bf841569.svg"
+  },
+  {
+    "revision": "dac67e7454b5f472b86c876b5694c428",
+    "url": "/static/media/trash.dac67e74.svg"
+  },
+  {
+    "revision": "f24489fce87bd0365e5c5fd2e0b21211",
+    "url": "/static/media/user.f24489fc.svg"
+  },
+  {
+    "revision": "f1bdfdc26535244535afa6b7fcc67be3",
+    "url": "/static/media/vietjetair_icon_400x400.f1bdfdc2.svg"
+  },
+  {
+    "revision": "f7bb30152b1fe7981500a33d0f00cc23",
+    "url": "/static/media/vietnamairlines_icon_400x400.f7bb3015.svg"
+  }
+]);
